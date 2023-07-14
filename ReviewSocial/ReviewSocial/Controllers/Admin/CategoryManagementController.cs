d39fa85cf913f396a526f1af62e4345097489430
NguyenLe0508 <nguyenthile050801@gmail.com>
2023-07-14 17:26:31 +0700

    layoutadmin

---
+++ ReviewSocial/ReviewSocial/Controllers/Admin/CategoryManagementController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReviewSocial.Controllers.Admin
+{
+    [Route("admin")]
+    public class CategoryManagementController : Controller
+    {
+        private readonly string view = "~/Views/Admin/CategoryManagement/";
+
+        [Route("category")]
+        public IActionResult Index()
+        {
+            return View(view+ "Index.cshtml");
+        }
+    }
+}
---
+++ ReviewSocial/ReviewSocial/Controllers/Admin/HomeAdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReviewSocial.Controllers.Admin
+{
+    [Route("admin")]
+    public class HomeAdminController : Controller
+    {
+        private readonly string view = "~/Views/Admin/HomeAdmin/";
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(view + "Index.cshtml");
+        }
+    }
+}
